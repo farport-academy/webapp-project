248ce27d318dd2799cf513b387977bb025d0db67
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-06-07 13:35:47 +0200

    feat: rxjs

--- src/app/pages/components/home/home.component.ts
+++ src/app/pages/components/home/home.component.ts
@@ -1,44 +1,85 @@
 import { Component } from '@angular/core';
+import { Observable, Subject, map, of, takeUntil } from 'rxjs';
 
 type TaskItem = {
-  id: number,
-  title: string,
-  done: boolean
-}
+  id: number;
+  title: string;
+  done: boolean;
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   a: number = 6;
   b: number = 6;
-
+  // creo il subject
+  destroy = new Subject<boolean>();
   taskList: TaskItem[] = [
     {
       id: 1,
       title: 'fare la spesa',
-      done: true
-    }, 
+      done: true,
+    },
     {
       id: 2,
-      title: 'Gettare l\'immondizia',
-      done: false
-    }, 
+      title: "Gettare l'immondizia",
+      done: false,
+    },
     {
       id: 3,
       title: 'Annaffiare le piante',
-      done: false
+      done: false,
     },
     {
       id: 4,
       title: 'Preparare il pranzo',
-      done: false
-    }
-  ]
+      done: false,
+    },
+  ];
 
-  trackById(index:number, item:TaskItem){
-    return item.id
+  counter: number | unknown = 0
+
+
+
+  ngOnInit() {
+    const observable = new Observable(
+      subscriber => {
+        let counter = 0
+        setInterval(
+          () =>{
+            subscriber.next(counter++)
+          }, 1000
+        )
+        // subscriber.complete()
+      }
+    )
+
+    // mi sottorcrivo all'observable
+    observable.pipe(
+      takeUntil(this.destroy)
+     
+    ).subscribe(
+      res =>{
+        this.counter = res
+      }
+    )
+
+  const source = of(1, 2, 3, 4, 5);
+  const example = source.pipe(map(val => val * 2));
+
+example.subscribe(val => console.log(val));
+
+  }
+
+  ngOnDestroy(){
+    // emetto il valore al destroy
+    this.destroy.next(true)
+  }
+
+  trackById(index: number, item: TaskItem) {
+    return item.id;
   }
 }
